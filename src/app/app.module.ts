3b99d5506e463480a82d875a6930a76ee7282426
AngeloDima <Angelodimauro91@gmail.com>
2023-10-04 13:51:54 +0200

    Base NavBar + 1Sec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CiaoGianniComponent } from './home/ciao-gianni/ciao-gianni.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    CiaoGianniComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/ciao-gianni/ciao-gianni.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ciao-gianni',
+  templateUrl: './ciao-gianni.component.html',
+  styleUrls: ['./ciao-gianni.component.css']
+})
+export class CiaoGianniComponent {
+
+}
