90169186ef3aa5214eafee1d351dc3b73665e599
AngeloDima <Angelodimauro91@gmail.com>
2023-10-05 12:16:50 +0200

    transizione CiaoGianni

--- src/app/home/ciao-gianni/ciao-gianni.component.ts
+++ src/app/home/ciao-gianni/ciao-gianni.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ciao-gianni',
   templateUrl: './ciao-gianni.component.html',
   styleUrls: ['./ciao-gianni.component.css']
 })
-export class CiaoGianniComponent {
+export class CiaoGianniComponent implements OnInit {
+  tran: boolean = false;
+  tran2: boolean = false;
+  tran3: boolean = false;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("START");
+
+    setTimeout(() => {
+      this.tran = true; // Attiva la transizione per tran dopo 200 millisecondi
+    }, 200);
+
+    setTimeout(() => {
+      this.tran2 = true; // Attiva la transizione per tran2 dopo 400 millisecondi
+    }, 500);
+
+    setTimeout(() => {
+      this.tran3 = true; // Attiva la transizione per tran2 dopo 400 millisecondi
+    }, 900);
+  }
 }
