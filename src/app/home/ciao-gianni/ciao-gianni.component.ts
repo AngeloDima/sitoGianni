7f8f157a5adc16d9f64f96aa2becbd8e47bbf847
AngeloDima <Angelodimauro91@gmail.com>
2023-10-05 18:23:31 +0200

    animazione 2.0

--- src/app/home/ciao-gianni/ciao-gianni.component.ts
+++ src/app/home/ciao-gianni/ciao-gianni.component.ts
@@ -11,35 +11,37 @@ export class CiaoGianniComponent implements OnInit {
   tran3: boolean = false;
 
   scrolledPixels: number = 0;
-  isScrolled: boolean = false; // Variabile per tenere traccia del superamento dei 90 pixel di scroll
+  isScrolled: boolean = false;
+  showContainer2: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
     console.log("START");
 
     setTimeout(() => {
-      this.tran = true; // Attiva la transizione per tran dopo 200 millisecondi
+      this.tran = true;
     }, 200);
 
     setTimeout(() => {
-      this.tran2 = true; // Attiva la transizione per tran2 dopo 400 millisecondi
+      this.tran2 = true;
     }, 500);
 
     setTimeout(() => {
-      this.tran3 = true; // Attiva la transizione per tran2 dopo 400 millisecondi
+      this.tran3 = true;
     }, 900);
   }
 
   @HostListener('window:scroll', ['$event'])
   onScroll(event: Event): void {
     this.scrolledPixels = window.scrollY;
-    this.isScrolled = this.scrolledPixels >= 4; // Imposta isScrolled in base alla condizione
-    console.log('Scrolled Pixels:', this.scrolledPixels);
+    this.isScrolled = this.scrolledPixels >= 4;
+
+    // Aggiungi questa condizione per mostrare il container2 quando si scorre oltre i 4 pixel
+    if (this.scrolledPixels >= 5) {
+      this.showContainer2 = true;
+    } if (this.scrolledPixels < 5) {
+      this.showContainer2 = false;
+    }
   }
-
-
-
 }
-
-
-
