69f5138e19b89114498528fb4f847306cc286278
AngeloDima <Angelodimauro91@gmail.com>
2023-10-06 19:02:37 +0200

    da sistemare il container che sbuca e quello nuovo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CiaoGianniComponent } from './home/ciao-gianni/ciao-gianni.component';
+import { AltreInfoComponent } from './home/altre-info/altre-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavBarComponent,
-    CiaoGianniComponent
+    CiaoGianniComponent,
+    AltreInfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/altre-info/altre-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-altre-info',
+  templateUrl: './altre-info.component.html',
+  styleUrls: ['./altre-info.component.css']
+})
+export class AltreInfoComponent {
+
+}
