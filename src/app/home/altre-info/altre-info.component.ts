50d53eded3959dac8d2fbec8ca00ff133399930d
AngeloDima <Angelodimauro91@gmail.com>
2023-10-08 11:08:10 +0200

    cambio di font, add img, add transizione, fix container (altre info)

--- src/app/home/altre-info/altre-info.component.ts
+++ src/app/home/altre-info/altre-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-altre-info',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./altre-info.component.css']
 })
 export class AltreInfoComponent {
+  scrolledPixels: number = 0;
+  showContainer2: boolean = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    this.scrolledPixels = window.scrollY;
+    console.log(this.scrolledPixels);
+
+    if (this.scrolledPixels >= 570) {
+      this.showContainer2 = true;
+    }
+  }
 }
