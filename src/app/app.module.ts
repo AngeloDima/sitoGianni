c16294469c9fdd508725b32082d8da4cae9019ef
AngeloDima <Angelodimauro91@gmail.com>
2023-10-09 11:53:12 +0200

    slider v1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CiaoGianniComponent } from './home/ciao-gianni/ciao-gianni.component';
 import { AltreInfoComponent } from './home/altre-info/altre-info.component';
+import { SliderComponent } from './home/slider/slider.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AltreInfoComponent } from './home/altre-info/altre-info.component';
     HomeComponent,
     NavBarComponent,
     CiaoGianniComponent,
-    AltreInfoComponent
+    AltreInfoComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/slider/slider.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.css']
+})
+export class SliderComponent {
+
+  scritte: string[] = [
+    "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista", "professore", "violinista"
+  ]
+
+
+}
+
+
+
