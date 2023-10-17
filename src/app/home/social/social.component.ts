b840b9bc16a766f09254b298d76e8d9214348b0f
AngeloDima <Angelodimauro91@gmail.com>
2023-10-17 17:48:03 +0200

    metto i vari collegamenti ai social

--- src/app/home/social/social.component.ts
+++ src/app/home/social/social.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class SocialComponent {
 
+
+  Instagram() {
+    window.open('https://www.instagram.com/il_violinista_di_tik_tok/', '_blank')
+  }
+
+  Facebook() {
+    window.open('https://www.facebook.com/giovanni.dimauro.33', '_blank')
+  }
+
+  TikTok() {
+    window.open('www.tiktok.com', '_blank')
+  }
+
+  WhatsApp() {
+    window.alert("numero")
+  }
+
 }
