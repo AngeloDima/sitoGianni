7eb88bc3b4948202f6eb882c433dd5eeb0bd38ac
AngeloDima <Angelodimauro91@gmail.com>
2023-10-18 11:04:11 +0200

    NavBar funzione Click :D

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,4 +7,84 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  ChiSono() {
+
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 2.65;
+
+
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth'
+    });
+  }
+
+
+  ChiSono2() {
+
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 3.05;
+
+
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth'
+    });
+  }
+
+
+  LAVORO() {
+
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 1.4;
+
+
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth'
+    });
+  }
+
+
+  LAVORO2() {
+
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 1.85;
+
+
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth'
+    });
+  }
+
+
+
+  CONTATTAMI() {
+
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 3.70;
+
+
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth'
+    });
+  }
+
+
+  CONTATTAMI2() {
+
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 5.5;
+
+
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth'
+    });
+  }
+
 }
+
+
